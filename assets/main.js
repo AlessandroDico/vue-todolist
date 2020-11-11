db9b5eba3b19484c0b66c1523e6ba6ec0f06a370
Ale <alessandrochb@gmail.com>
2020-11-11 10:59:03 +0100

    sistemato titolo pagina

--- assets/main.js
+++ assets/main.js
@@ -5,10 +5,13 @@ al click su un item della lista, il todo viene rimosso
 BONUS: intercettare anche il tasto "ENTER" per aggiungere un nuovo todo
 */
 
-
+//1
 // definire una lista di todo e stamparli in pagina
 //fare un array di stringhe
 //stamparli in pagina tramite v-for
+
+//2
+//predisporre un input testuale per aggiungere un nuovo todo
 var app = new Vue ({
     el: '#root',
     data: {
