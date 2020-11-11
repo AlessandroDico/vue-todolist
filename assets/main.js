dfc8ed1344f3faa27fdd0e8cc6fc4c2aaf6bed84
Ale <alessandrochb@gmail.com>
2020-11-11 17:07:38 +0100

    aggiunta possibilità di segnare in rosso elementi da postporre

--- assets/main.js
+++ assets/main.js
@@ -17,18 +17,28 @@ var app = new Vue ({
     el: '#root',
     data: {
         todoList: [
-            'fare la spesa',
+//trasformo le singole stringhe in array con un testo e una classe
+//in questo modo è più facile selezionare i singoli elementi per 'postporli'
+            { text : 'fare la spesa', changeState : 'blue' },
+            { text : 'portare fuori il cane', changeState : 'blue' },
+            { text : 'studiare', changeState : 'blue' },
+            { text : 'allenarsi', changeState : 'blue' },
+            { text : 'cucinare', changeState : 'blue' },
+            { text : 'nerdare', changeState : 'blue' },
+            /*
             'portare fuori il cane',
             'studiare',
             'allenarsi',
             'cucinare',
             'nerdare',
+            */
         ],
         todoAdd: '',
+        changeState : 'blue'
     },
     methods: {
         pushInArray() {
-            this.todoList.push(this.todoAdd);
+            this.todoList.push({ text : this.todoAdd, changeState : 'red'});
             this.todoAdd = '';
         },
         deleteTodo(index) {
@@ -36,12 +46,24 @@ var app = new Vue ({
 // uso la funzione splice() per rimuovere un elemento
 //il valore 1 serve per rimuovere un elemento alla volta... se aumento il valore, allo scatenarsi della funzione(in questo caso con il click) mi toglie tanti elementi quandi il valore del numero (se parto dall'alto)
         },
+        
         /*
         //funzione alternativa per rimuovere un item
         deleteTodo(index) {
             this.$delete(this.todoList, index);
         }
         */
+
+        postponedItem(item) {
+            // console.log(item);
+
+            if (item.changeState == 'blue') {
+                item.changeState = 'red';
+            } else {
+                item.changeState = 'blue';
+            }
+        },
+
     },
 })
 
